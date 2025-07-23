f59a70e66ec400c937b0408659aa2cfa58276038
vvo8hc <daivietvonin1@gmail.com>
2025-07-23 23:46:02 +0700

    create auth entity and dockerize auth

---
+++ apps/auth/src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersRepository } from './users.repository';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+    constructor(private readonly usersRepository: UsersRepository){}
+    async create(createUserDto: CreateUserDto){
+        return this.usersRepository.create({
+            ...createUserDto
+        })
+    }
+
+    findAll() {
+        return this.usersRepository.find({});
+      }
+    
+      findOne(_id: string) {
+        return this.usersRepository.findOne({
+          _id
+        });
+      }
+    
+      update(_id: string, updateUserDto: UpdateUserDto) {
+        return this.usersRepository.findOneAndUpdate(
+          {_id},
+          {$set: updateUserDto},
+        )
+      }
+    
+      remove(_id: string) {
+        return this.usersRepository.findOneAndDelete({_id});
+      }
+}
